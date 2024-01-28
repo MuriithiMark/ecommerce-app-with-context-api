cfd19fb86a2599a5a51297399ad8d4b874492c7c
MuriithiMark <markmwangi14317@gmail.com>
2024-01-28 16:37:11 +0300

    minimal docs for utils functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,16 @@
+/**
+ * 
+ * @param { "dark" | "light" } theme 
+ */
 const changeColorScheme = async (theme) => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
 }
 
+/**
+ * 
+ * @returns  { Promise<"dark" | "light"> }
+ */
 const retriveTheme = async () => {
     let theme = localStorage.getItem("theme");
     if (!theme) {
